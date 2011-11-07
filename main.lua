8a062fbeeb8e7f4f90544739e4675b4529621917
Tom Bortels <bortels@gmail.com>
2011-11-07 15:49:01 -0800

    First Commit

---
+++ main.lua
@@ -0,0 +1,57 @@
+-- Just a "Main" tab for Codea to demo the class
+
+function setup()
+   f=Font()
+   f:init()
+   frame=1
+   offset = 0
+   stroke_width=2
+   iparameter("stroke_width", 1, 5, stroke_width)
+   speed=2
+   parameter("speed", 0, 8, speed)
+   x=50
+   text = "Why do you necessarily have to be wrong just because "
+       .. "a few million people think you are?     "
+    bannerscale= 2
+    parameter("bannerscale", 0, 10, bannerscale)
+   bannerlength = f:stringwidth(text, bannerscale)
+   spintxt = "Bananas!"
+   spinscale = 1.5
+   spinwidth = f:stringwidth(spintxt, spinscale)
+   rf = 0.03 -- rainbow frequency
+end
+
+function draw()
+   background(0, 0, 0, 255)
+   frame = frame + 1 -- used by lots of things
+   stroke(226, 235, 234, 255)
+   noSmooth()
+   strokeWidth(stroke_width)
+   lineCapMode(ROUND)
+   f:drawstring("Hershey Roman Simplex " .. frame, 150, 200)
+   -- smooth scroll
+   stroke(140, 147, 215, 255)
+   local bl = f:drawstring(text .. text,
+                           x - offset, HEIGHT/2,
+                           bannerscale)
+   offset = offset + speed
+   if (offset > bl) then offset = 0 end
+
+   -- changing size
+   fps = string.format("%.2f", frame / ElapsedTime)
+   f:drawstring(fps .. " fps", 500, 600, math.sin(frame / 94))
+
+   -- rainbow!
+   stroke(math.sin(rf*frame)*127 + 128,
+          math.sin(rf*frame + 2)*127 + 128,
+          math.sin(rf*frame + 4)*127 + 128, 255)
+    f:drawstring("Daddy loves Gracie and Maddy", 200, 20)
+    
+   -- spinner (using external positioning)
+   translate(200, 550)
+   rotate(frame*2.5)
+   translate(0-spinwidth/2, -10)
+   stroke(255, 252, 0, 255)
+   f:drawstring(spintxt, 0, 0, spinscale)
+
+end
